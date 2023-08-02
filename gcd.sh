e95cccc3661b8c9dd6d587cff8b40db0b1b39234
kabaki1982 <a2211tk@aiit.ac.jp>
2023-08-02 10:55:47 +0000

    create

---
+++ gcd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+function isNotNumeric() {
+  expr "$1" + 1 >/dev/null 2>&1
+  if [ $? -ge 2 ]; then
+    return 0
+  fi
+  return 1
+}
+
+tempa=$1
+tempb=$2
+
+if isNotNumeric ${tempa} ; then
+	echo "引数1が数字ではありません！"
+elif isNotNumeric ${tempb} ; then
+	echo "引数2が数字ではありません！"
+else
+
+#echo $tempa
+#echo $tempb
+remainder=$((tempa  % tempb))
+#echo $remainder
+while [ $((remainder)) -gt 0 ]
+do
+#	echo "Loop"
+	tempa=$tempb
+	tempb=$remainder
+	remainder=$((tempa % tempb))
+#	echo $remainder
+done
+#echo "end"
+#echo $1
+#echo $2
+echo "greatest common divisor: $tempb "
+fi
